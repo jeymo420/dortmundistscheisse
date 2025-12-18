9f644e74d5a0e3bddf6e99fd130f4e48bbf8aec8
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2025-12-19 00:25:15 +0100

    fix: enhance logging in onRequest middleware

--- src/middleware.js
+++ src/middleware.js
@@ -3,13 +3,17 @@ export function onRequest(context, next) {
     const hostname = url.hostname;
     const parts = hostname.split(".");
 
-    let subdomain = "Dortmund";
+    let subdomain = "";
     if (hostname !== "localhost" && !/^\d+\.\d+\.\d+\.\d+$/.test(hostname) && parts.length > 2) {
         subdomain = parts[0];
     }
 
     context.locals.website = `${url.protocol}//${hostname}`;
     context.locals.target = subdomain;
+
+    console.log(`Request for url: ${context.request.url}, subdomain: ${subdomain}`);
+    console.log(`Set context.locals.website to: ${context.locals.website}`);
+    console.log(`Set context.locals.target  to: ${context.locals.target}`);
 
     return next();
 }
